3ab9ffc7c2f6e2558977d2b7d14ee9e67a619977
NurniyazKazaliyev <97945131+NurniyazKazaliyev@users.noreply.github.com>
2022-02-23 16:06:13 +0600

    Delete Paint.java

--- src/com/company/Paint.java
+++
@@ -1,8 +0,0 @@
-function _(selector) {
-    return document.querySelector(selector);
-}
-function setup() {
-    let canvas = createCanvas(650, 600);
-    canvas.parent("canvas-wrapper");
-    background(255);
-}
